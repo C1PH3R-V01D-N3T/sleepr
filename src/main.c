0e67eacd3ebee394a72c9e2e049b018206f952ff
C1PH3R_V01D_N3T <gustavopablo.alves@outlook.com>
2026-06-14 14:51:47 -0300

    feat(style): Added banner for app

--- src/main.c
+++ src/main.c
@@ -2,18 +2,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-  int secs;
+int main(void) {
+    FILE *f = fopen("src/banner.txt", "r");
+    int c;
 
-  printf("Enter the time in seconds: ");
-  scanf("%d", &secs);
-  
-  printf("\nThe computer will suspend in %d seconds", secs);
-  fflush(stdout);
+    if (!f) {
+        perror("Erro ao abrir banner");
+        return 1;
+    }
 
-  sleep(secs);
+    while ((c = fgetc(f)) != EOF) {
+        putchar(c);
+    }
 
-  system("systemctl suspend");
+    fclose(f);
 
-  return 0;
+    int secs;
+
+    printf("\n\nEnter the time in seconds: ");
+
+    if (scanf("%d", &secs) != 1 || secs <= 0) {
+        printf("Invalid time.\n");
+        return 1;
+    }
+
+    printf("\nThe computer will suspend in %d seconds\n", secs);
+
+    for (int i = secs; i > 0; i--) {
+        printf("\rSuspending in %d...", i);
+        fflush(stdout);
+        sleep(1);
+    }
+
+    printf("\nSuspending now...\n");
+
+    system("systemctl suspend");
+
+    return 0;
 }
